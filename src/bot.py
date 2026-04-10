eff9b11ba8b90373939dcb1a62eb87fcc320923b
Denis Aleksandrov <69910477+lolkofka@users.noreply.github.com>
2026-04-10 21:07:00 +0300

    добавил поддержку прокси для телеграмма

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 from aiogram import Dispatcher, Bot, Router
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.mongo import MongoStorage
 
@@ -13,9 +14,17 @@ bot_properties = DefaultBotProperties(
     link_preview_is_disabled=True,
     parse_mode=ParseMode.HTML
 )
-main_bot = Bot(config['bot']['token'], default=bot_properties)
 
-dispatcher = Dispatcher(storage=MongoStorage(database.client, config['bot']['database']))
+proxy_url = config["bot"]["proxy"]
+session = AiohttpSession(proxy=proxy_url)
+
+main_bot = Bot(
+    config["bot"]["token"],
+    default=bot_properties,
+    session=session,
+)
+
+dispatcher = Dispatcher(storage=MongoStorage(database.client, config["bot"]["database"]))
 dispatcher.include_router(router)
 dispatcher.include_router(admin)
 
